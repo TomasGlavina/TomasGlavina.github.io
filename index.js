8c35253e1a4ca4aee7e30e8db6a1959940f9e1cd
Tomas Glavina <tomi.glavina@gmail.com>
2022-10-06 17:16:10 +0300

    Finished project pages. TODO: add videos to youtube and fetch them from there

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-const menu = document.querySelector(".menu");
-const close = document.querySelector(".close");
-const nav = document.querySelector("nav");
-const hero = document.querySelector("hero");
+let showError = () => {
+    const video = document.getElementById("video-example");
+    video.src = "videos/error-sample.webm";
+    video.setAttribute("type", "video/webm");
+    const errorTab = document.getElementById("error");
+    errorTab.classList.add("active");
+    errorTab.classList.add("bg-primary");
+    errorTab.classList.add("text-black");
+    errorTab.classList.add("bg-opacity-75");
+    const exampleTab = document.getElementById("example");
+    exampleTab.classList.remove("active");
+    exampleTab.classList.remove("bg-primary");
+    exampleTab.classList.remove("text-black");
+};
 
-
-menu.addEventListener('click', () => {
-    nav.classList.add('open-nav');
-});
-
-close.addEventListener('click', () =>{
-    nav.classList.remove('open-nav');
-});
-
+let showExample = () => {
+    const video = document.getElementById("video-example");
+    video.src = "videos/hotel-sample.webm";
+    video.setAttribute("type", "video/webm");
+    const errorTab = document.getElementById("error");
+    errorTab.classList.remove("active");
+    errorTab.classList.remove("bg-primary");
+    errorTab.classList.remove("text-black");
+    const exampleTab = document.getElementById("example");
+    exampleTab.classList.add("active");
+    exampleTab.classList.add("bg-opacity-75");
+    exampleTab.classList.add("bg-primary");
+    exampleTab.classList.add("text-black");
+};
