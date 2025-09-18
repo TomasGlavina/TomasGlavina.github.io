73d94e0c55a3ca67742d4fa0b2c6a3729b4af94d
Tomas Glavina <tomi.glavina@gmail.com>
2025-09-18 15:57:16 +0300

    removed sorting from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,30 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Projects from './pages/Projects';
-import './App.css'
-import Navbar from './components/Navbar';
-import './index.css';
-import Contact from './pages/Contact';
-import SortingVisualizer from './pages/SortingVisualizer';
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import "./index.css";
+import Contact from "./pages/Contact";
+//import SortingVisualizer from './pages/SortingVisualizer';
 
 function App() {
   return (
-    <div className='pt-20 mocha text-text bg-base '>
+    <div className="pt-20 mocha text-text bg-base ">
       <Router>
         <nav>
           <Navbar />
         </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/sorting-visualizer" element={<SortingVisualizer />} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
